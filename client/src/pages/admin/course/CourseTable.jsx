c1ad1798c7e8ac5f7a498585d563057b58cab947
Sandeep Pal <sandeeppal6926@gmail.com>
2026-01-06 16:20:09 +0530

    created the course upload feature

--- client/src/pages/admin/course/CourseTable.jsx
+++ client/src/pages/admin/course/CourseTable.jsx
@@ -11,6 +11,11 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { useNavigate } from "react-router-dom";
+import {  useGetCreatorCourseQuery } from "@/features/api/courseApi";
+import { Edit } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+
 
 const invoices = [
   {
@@ -58,12 +63,17 @@ const invoices = [
 ];
 
 const CourseTable = () => {
+  const { data, isLoading } = useGetCreatorCourseQuery();
+
   const navigate = useNavigate();
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
     <div>
-      <Button onClick={()=> navigate(`create`)}>Create a New Course</Button>
+      <Button onClick={() => navigate(`create`)}>Create a New Course</Button>
       <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableCaption>A list of your recent courses.</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">Price</TableHead>
@@ -73,26 +83,21 @@ const CourseTable = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
+          {data.courses.map((course) => (
+            <TableRow key={course._id}>
+              <TableCell className="font-medium">{course?.coursePrice || "NA"}</TableCell>
+              <TableCell><Badge>{course.isPublished? "Published" : "Draft"}</Badge></TableCell>
+              <TableCell>{course.courseTitle}</TableCell>
               <TableCell className="text-right">
-                {invoice.totalAmount}
+                <Button size="sm" variant="ghost"><Edit/></Button>
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
-        </TableFooter>
       </Table>
     </div>
   );
 };
+
 
 export default CourseTable;
